443132740e727bcfd8569bbcf41acc287815a8a2
Adtya Gupta <adtyagupta72@gmail.com>
2026-06-24 13:27:26 +0530

    React: Props and Todo App

--- react-practice/src/TestComp.jsx
+++ react-practice/src/TestComp.jsx
@@ -1,7 +1,14 @@
-// let TestComp = () =>
+// let TestComp = (props) =>
 // {
-//     return <div>
-//         <h1>This is my functional Component!</h1>
+//     console.log(props)
+
+
+
+//     return <div style={{backgroundColor: props.color}}>
+//         {/* <h1>This is my Class Component!</h1>
+//         <br></br>
+//         <button onClick={props.clickFunction}>Click me!</button> */}
+
 //     </div>
 // }
 
@@ -14,7 +21,10 @@ class TestComp extends React.Component
         super(props)
         console.log("Props:", props)
     }
-    state = {count: this.props.count}  
+    state = {
+        count: this.props.count,
+        todoList: []
+    }
 
     incrementCount()
     {
@@ -22,14 +32,41 @@ class TestComp extends React.Component
         this.setState({count: this.state.count+1})//
         console.log("Count: ", this.state.count)//
     }
+
+    addTodo()
+    {
+        var todo = document.getElementById("newTodo").value
+        console.log("todo:", todo)
+        var todoObject = {
+            title:todo,
+            id:this.state.todoList.length+1
+        }
+        let tempTodoList = this.state.todoList
+        tempTodoList.push(todoObject)
+        this.setState({...this.state, todoList: tempTodoList})
+    }
+
     render()
     {
         return <div style={{backgroundColor: this.props.color}}>
-                <h1>This is my Class Component! {this.state.count}</h1>
+                {/* <h1>This is my Class Component! {this.state.count}</h1>
                 <br></br>
                 <button onClick={this.props.clickFunction}>Click me!</button>
                 <button onClick={()=>this.incrementCount()}>Increment count</button>
-                <button onClick={()=>console.log(this.state.count)}>Check count</button>
+                <button onClick={()=>console.log(this.state.count)}>Check count</button> */}
+                <h1>To Do App</h1>
+                <br/>
+                <input placeholder="Add todo here..." id="newTodo"/>
+                <button onClick={()=>this.addTodo()}>Add Todo</button>
+                <br/>
+                <ul id="todoListID">
+                    {this.state.todoList.map((tempTodo)=>{
+                        return <li>
+                            <label>{tempTodo.title}</label>
+                        </li>
+                    })}
+                </ul>
+                <button onClick={console.log(this.state.todoList)}>Test Todo List</button>
             </div>
     }
 }
